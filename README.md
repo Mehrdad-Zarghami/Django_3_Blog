f910db09578a20d270c2f9b09f4c9fbec0a44df2
Mehrdad <mehrdad.zarghami69@gmail.com>
2022-10-06 17:01:37 +0330

    Create PostModel and use ForeignKey for author field

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Django_3_Blog
-A blog that people can upload their posts and delete or edit them and also read other's posts.  
+A blog that people can upload their posts and delete or edit them and also read other's posts.
+
+Initialization --> in terminal:
+1. conda create -n blog_django python=3.10
+
+2. conda install django
+
+3. django-admin startproject config .
+
+4. python manage.py startapp blog
+
+5. python manage.py migrate --> the default migrations that allow us to create superuser
+
+6. python manage.py createsuperuser --> create a superuser that have access to the admin panel
+7. 
+
---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class PostModel(models.Model):
+    SATUS_CHOISES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+
+    title = models.CharField(max_length=100)
+    text = models.TextField()  # no limitation for maximum length of this field
+    # Use the PrimaryKey (pk or id) of 'User' model from 'auth' app, as the ForeignKey of PostModel table:
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    datatime_create = models.DateTimeField(auto_now_add=True)  # a fix datetime at the time of post creation
+    datetime_modified = models.DateTimeField(auto_now=True)  # a datetime that changes everytime we edit a post
+    status = models.CharField(choices=SATUS_CHOISES, max_length=3)
+
+
+
