d625bc5052c69acfeaa46e2f4294d71c06c41705
Mehrdad <mehrdad.zarghami69@gmail.com>
2022-10-19 15:01:14 +0330

    setUpTestData(cls) insted of setUp(self) in test.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,26 +1,49 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
-from .models import PostModel 
+from .models import PostModel
 
 
 class TestBlogPost(TestCase):
-    def setUp(self):
-        self.user1 = User.objects.create(username='Mehrdad Test')
-        self.post1 = PostModel.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        """
+        These samples are created only once, not before each test unlike setUp(self).
+        If we change a test sample in a test, the change remains for next tests.
+        """
+
+        cls.user1 = User.objects.create(username='Mehrdad Test')
+        cls.post1 = PostModel.objects.create(
             title='Test Title1',
             text='Description for Testing Post1',
             status=PostModel.STATUS_CHOICES[0][0],
-            # status = pub
-            author=self.user1,
+            # status = 'pub'
+            author=cls.user1,
         )
-        self.post2 = PostModel.objects.create(
+        cls.post2 = PostModel.objects.create(
             title='Test Title2',
             text='Description for Testing Post1',
             status=PostModel.STATUS_CHOICES[1][0],
             # status = 'drf'
-            author=self.user1,
+            author=cls.user1,
         )
+
+    # def setUp(self):
+    #     self.user1 = User.objects.create(username='Mehrdad Test')
+    #     self.post1 = PostModel.objects.create(
+    #         title='Test Title1',
+    #         text='Description for Testing Post1',
+    #         status=PostModel.STATUS_CHOICES[0][0],
+    #         # status = pub
+    #         author=self.user1,
+    #     )
+    #     self.post2 = PostModel.objects.create(
+    #         title='Test Title2',
+    #         text='Description for Testing Post1',
+    #         status=PostModel.STATUS_CHOICES[1][0],
+    #         # status = 'drf'
+    #         author=self.user1,
+    #     )
 
     def test_posts_list_url(self):  # The name of a test function must start with 'test'
         # Check if '/blog/' url works or not
@@ -53,7 +76,7 @@ class TestBlogPost(TestCase):
     def test_status_404_if_post_does_not_exist(self):
         response = self.client.get(reverse('post_detail_page', args=[999]))
         self.assertEqual(response.status_code, 404)
-        
+
     def test_draft_posts_not_in_posts_list_page(self):
         response = self.client.get(reverse('posts_list_page'))
         self.assertContains(response, self.post1.title),
